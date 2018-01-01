482907e1a05c1cdf100b689f9b64c068e1997015
xiaogui-2017 <923445457@qq.com>
2018-01-01 14:38:23 +0800

    用默认参数在定义内部函数时，去获取enclosing的 参数a

--- high_level_lxf/fix_return_bug.py
+++ high_level_lxf/fix_return_bug.py
@@ -12,13 +12,34 @@ def count():
             :param j:
             :return:
             """
+
             def g():
-                return j*j
+                return j * j
+
             return g
+
         # 把i传给内部函数f,而不是直接塞给他, 内部函数一定要接受参数,
         r = f(i)  # i给啦line 9的j
         fs.append(r)
     return fs
 
+
+def count2():
+    """
+    问题的产生是因为函数只在执行时才去获取外层参数i，若函数定义时可以获取到i，问题便可解决。
+    而默认参数正好可以完成定义时获取i值且运行函数时无需参数输入的功能，所以在函数f()定义中改为f(m = i),函数f返回值改为m*m即可.
+    :return:
+    """
+    fs = []
+    for i in range(1, 4):
+        def f(m=i):
+            return m * m
+        fs.append(f)
+    return fs
+
+
 f1, f2, f3 = count()
+f4, f5, f6 = count2()
 print f1(), f2(), f3()
+print f4(), f5(), f6()
+
