c90fdd8c2ce8bdbd4c1ea55263a1777f5e6f8b5b
xiaogui-2017 <923445457@qq.com>
2018-01-01 18:20:16 +0800

    带参数的三层装饰器j

---
+++ high_level_lxf/runtime_unit_decrator.py
@@ -0,0 +1,28 @@
+# -*- coding=utf-8 -*-
+import time
+
+
+def performance(ms):
+    """
+    @performace增加一个参数，允许传入's'或'ms'：
+    :param ms:
+    :param unit:
+    :return:
+    """
+    def ourter(unit):
+        def wraper(*args, **kwargs):
+            s_time = time.time()
+            r = unit(*args, **kwargs)
+            e_time = time.time()
+            # 之前的思路不变， 装饰器的参数ms,最内部函数依然能够调用
+            return '执行的时间是%s%s' % (e_time-s_time, ms)
+        return wraper
+    return ourter
+
+
+@performance('ms')
+def factorial(n):
+    return reduce(lambda x, y: x * y, range(1, n + 1))
+
+
+print factorial(10)
