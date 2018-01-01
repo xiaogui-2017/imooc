127fe9331706e341297c2c7457f85674f4acd886
xiaogui-2017 <923445457@qq.com>
2018-01-01 17:13:01 +0800

    匿名函数

--- high_level_lxf/lambda.py
+++ high_level_lxf/lambda.py
@@ -5,3 +5,5 @@ def is_not_empty(s):
 
 # 不用遍历后边的列表
 print filter(lambda i: i and len(i.strip()) > 0, ['test', None, '', 'str', '  ', 'END'])
+
+print filter(lambda x: x and len(x.strip()) > 0, ['test', None, '', 'str', '  ', 'END'])
